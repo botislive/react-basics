aa48e7caef7f50cdc1daa55ef9c71ff2648f3411
botislive <tushardtar704@gmail.com>
2026-02-11 18:01:03 +0530

    Integrated tailwind css and worked on props in react

---
+++ props/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+import React from 'react'
+import { Card } from './components/Card';
+
+const App = () => {
+  return (
+    <>
+        <Card username="Tushar"/>
+        <Card username="Pummy"/>
+        <Card username="Harshini"/>
+    </>
+  );
+}
+
+
+export default App
